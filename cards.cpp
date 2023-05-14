853e396cb2a27471dc3f6aa7753d4bf52a076f5f
Marina Lebkova <m.lebkova@gmail.com>
2023-05-14 21:25:40 +0300

    remove from deck function

--- cards.cpp
+++ cards.cpp
@@ -21,14 +21,34 @@ public:
 
   ~Deck() = default;
 
-  void AddToDeck(int card_id, std::string deck);
+  void AddToDeck(int card_id, std::string deck_name);
+  void RemoveFromDeck(int card_id, std::string deck_name);
 };
 
 void Deck::AddToDeck(int card_id, std::string deck_name) {
   std::ifstream questions(path_to_file_);
   json data = json::parse(questions);
 
-  data["cards"][card_id-1]["deck"].push_back(deck_name);
+  data["cards"][card_id - 1]["deck"].push_back(deck_name);
+
+  std::ofstream questions_edit("cards.json");
+  questions_edit << std::setw(2) << data << std::endl;
+
+  questions.close();
+  questions_edit.close();
+}
+
+void::Deck::RemoveFromDeck(int card_id, std::string deck_name) {
+  std::ifstream questions(path_to_file_);
+  json data = json::parse(questions);
+
+  int deck_count = data["cards"][card_id - 1]["deck"].size();
+  for (int deck_idx = 0; deck_idx <= deck_count; deck_idx += 1) {
+    if (data["cards"][card_id - 1]["deck"][deck_idx] == deck_name) {
+      data["cards"][card_id - 1]["deck"].erase(deck_idx);
+      break;
+    }
+  }
 
   std::ofstream questions_edit("cards.json");
   questions_edit << std::setw(2) << data << std::endl;
@@ -40,5 +60,6 @@ void Deck::AddToDeck(int card_id, std::string deck_name) {
 int main() {
   Deck test;
   test.AddToDeck(1, "maths");
+  test.RemoveFromDeck(2, "hehe");
   return 0;
 }
