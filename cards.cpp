46ad83a15f0c387ee9ab821a465d133c18ab7deb
Marina Lebkova <m.lebkova@gmail.com>
2023-05-14 22:16:41 +0300

    i love cats

--- cards.cpp
+++ cards.cpp
@@ -6,60 +6,77 @@
 
 using json = nlohmann::ordered_json;
 
-class Deck {
-private:
+class CardManager {
+public:
   std::string path_to_file_ = "cards.json";
 
 public:
-  Deck() {
+  CardManager() {
     path_to_file_ = "cards.json";
   }
 
-  Deck(std::string& path)
+  CardManager(const CardManager&) {};
+
+  CardManager(std::string& path)
     : path_to_file_{ path }
   {}
 
-  ~Deck() = default;
+  ~CardManager() = default;
+
+  json ReadFromCardsFile(std::string& path);
+  void WriteToCardsFile(std::string& path, json& data);
 
   void AddToDeck(int card_id, std::string deck_name);
   void RemoveFromDeck(int card_id, std::string deck_name);
 };
 
-void Deck::AddToDeck(int card_id, std::string deck_name) {
-  std::ifstream questions(path_to_file_);
-  json data = json::parse(questions);
-
-  data["cards"][card_id - 1]["deck"].push_back(deck_name);
-
-  std::ofstream questions_edit("cards.json");
-  questions_edit << std::setw(2) << data << std::endl;
-
-  questions.close();
-  questions_edit.close();
+// чтение из json
+json CardManager::ReadFromCardsFile(std::string& path) {
+  std::ifstream cards_file(path);
+  json data = json::parse(cards_file);
+  cards_file.close();
+  return data;
 }
 
-void::Deck::RemoveFromDeck(int card_id, std::string deck_name) {
-  std::ifstream questions(path_to_file_);
-  json data = json::parse(questions);
+// запись в json
+void CardManager::WriteToCardsFile(std::string& path, json& data) {
+  std::ofstream cards_edit(path);
+  cards_edit << std::setw(2) << data << std::endl;
+  cards_edit.close();
+}
 
-  int deck_count = data["cards"][card_id - 1]["deck"].size();
-  for (int deck_idx = 0; deck_idx <= deck_count; deck_idx += 1) {
-    if (data["cards"][card_id - 1]["deck"][deck_idx] == deck_name) {
-      data["cards"][card_id - 1]["deck"].erase(deck_idx);
+// добавление карточки в колоду
+void CardManager::AddToDeck(int card_id, std::string deck_name) {
+  json data = CardManager::ReadFromCardsFile(path_to_file_);
+
+  data["cards"][card_id]["deck"].push_back(deck_name);
+
+  CardManager::WriteToCardsFile(path_to_file_, data);
+}
+
+// удаление карточки из колоды
+void CardManager::RemoveFromDeck(int card_id, std::string deck_name) {
+  json data = CardManager::ReadFromCardsFile(path_to_file_);
+
+  // количество колод, в которых есть карточка
+  int deck_count = data["cards"][card_id]["deck"].size();
+
+  // ищем индекс колоды, чтобы стереть её
+  bool deleted = 0;
+  for (int deck_idx = 0; deck_idx < deck_count; deck_idx += 1) {
+    if (data["cards"][card_id]["deck"][deck_idx] == deck_name) {
+      data["cards"][card_id]["deck"].erase(deck_idx);
+      deleted = 1;
       break;
     }
   }
 
-  std::ofstream questions_edit("cards.json");
-  questions_edit << std::setw(2) << data << std::endl;
-
-  questions.close();
-  questions_edit.close();
+  CardManager::WriteToCardsFile(path_to_file_, data);
 }
 
 int main() {
-  Deck test;
+  CardManager test;
   test.AddToDeck(1, "maths");
-  test.RemoveFromDeck(2, "hehe");
+  test.RemoveFromDeck(1, "hehe");
   return 0;
 }
